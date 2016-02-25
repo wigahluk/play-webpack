5b81bea72738b3c0814c00f322432a59b242f5b6
Oscar Ponce <wigahluk@gmail.com>
2016-02-25 15:28:38 -0800

    Trying to get this working with Angular 1.5

--- app/frontend/app/main.ts
+++ app/frontend/app/main.ts
@@ -1,14 +1,12 @@
 import * as angular from 'angular'
-//import controller from './controller';
+import controller from './controller';
 
 const app = angular.module('app', []);
 
 
 
 //var app = angular.module('app', []);
-app.controller('greetings', function($scope) {
-    $scope.name = 'Welcome!';
-});
+app.controller('greetings', controller);
 console.log('pre-ready');
 angular.element(document).ready(() => {
     console.log('ready');
